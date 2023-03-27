22dae86203850e6d0037c78548ac9b19ecca4226
seunghaen <tmdgus9701@naver.com>
2023-03-27 14:56:02 +0900

    model setting

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
 const express = require("express");
-const app = express();
+const cookeiParser = require("cookie-parser");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { sequelize } = require("./models");
 
-app.get("/", (req, res, next) => {
-  res.send("hello world");
+dotenv.config();
+
+const authRouter = require("./routes/auth");
+const { Sequelize } = require("sequelize");
+
+const app = express();
+app.set("port", process.env.PORT || 8001);
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookeiParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+app.use("/auth", authRouter);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method},${req.url} 라우터 없음`);
+  error.status = 404;
+  next(error);
 });
-app.listen(8001, (req, res, next) => {
-  console.log("8001번에 서버연결");
+
+app.use((err, req, res, next) => {
+  console.error(err);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`${app.get("port")}번 포트에서 서버 연결중`);
 });
